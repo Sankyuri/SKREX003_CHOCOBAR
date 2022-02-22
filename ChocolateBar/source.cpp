637897a0969b3176ebd23b02e254b851080724e4
Sankyuri <54235470+Sankyuri@users.noreply.github.com>
2022-02-22 18:56:42 +0900

    Changed some dissatisfaction point.

--- ChocolateBar/source.cpp
+++ ChocolateBar/source.cpp
@@ -1,11 +1,29 @@
+/*
+ * 
+ *  Created by Sankyuri
+ * 
+ *  Attention:
+ *  - Written by Japanese.
+ *  - Required C++20.
+ * 
+ *  License:
+ *  This program has not license.
+ *  You can free use, copy and etc at all.
+ * 
+ * 
+ */
+
 #include <iostream>
 #include <string>
+
 
 // ********  using 宣言  ********
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+
+
 
 
 // ********  定数宣言  ********
@@ -17,52 +35,52 @@ static constexpr int WRAPPING_HEIGHT = 6;
 // ********  プロトタイプ宣言  ********
 
 // 板チョコの大きさを入力する
-void input_chocolate_bar_size(
-	int &row,
-	int &column
+void get_chocolate_bar_size(
+		int &column,
+		int &row
 );
 
 
 // 板チョコを表示する
 void print_chocolate_bar(
-	int row,
-	int column
+		int column,
+		int row
 );
 
 // 板チョコの頭を表示する
 void print_chocolate_bar_head(
-	int row
+		int column
 );
 
 // 板チョコの中身を表示する
 void print_chocolate_bar_body(
-	int row
+		int column
 );
 
 // 板チョコの包装紙を表示する
 void print_chocolate_bar_wrapping(
-	int row
+		int column
 );
 
 
 // 左端+真ん中+右端で文字列を表示する
-void print_3_string_row_times(
-	int    row,
-	string left,
-	string center,
-	string right
+void print_chocolate_bar_line(
+		int    column,
+		string left,
+		string center,
+		string right
 );
 
 
 // 左端+真ん中+右端で飾り付きの部分の文字列を表示する
-void print_3_string_row_times_with_decoration(
-	int    row,
-	string left,
-	string left_decolated,
-	string center,
-	string center_adjust,
-	string right,
-	string right_decolated
+void print_chocolate_bar_line_decorated(
+		int    column,
+		string left,
+		string left_decolated,
+		string center,
+		string center_adjust,
+		string right,
+		string right_decolated
 );
 
 
@@ -76,29 +94,23 @@ void print_3_string_row_times_with_decoration(
 
 int main()
 {
-	// **** 変数宣言 ****
+	// **** 変数 ****
 
 	// 横の数
-	int row    = 0;
-	// 縦の数
 	int column = 0;
+	// 縦の数
+	int row    = 0;
 
 
-	// **** 入力 ****
-	input_chocolate_bar_size(
-		row,
-		column
-	);
-
-
+	// **** 大きさの入力 ****
+	get_chocolate_bar_size( column, row );
+	
+	// 1行空ける
+	cout << endl;
 
 
 	// **** 板チョコの出力 ****
-	
-	print_chocolate_bar(
-		row,
-		column
-	);
+	print_chocolate_bar( column, row );
 
 
 }
@@ -111,18 +123,18 @@ int main()
 
 
 // 板チョコの大きさを入力する
-void input_chocolate_bar_size(
-	int &row,
-	int &column
+void get_chocolate_bar_size(
+		int &column,
+		int &row
 )
 {
 	// 横の数
-	cout << "row?: ";
-	cin >> row;
+	cout << "column?: ";
+	cin  >> column;
 
 	// 縦の数
-	cout << "column?: ";
-	cin >> column;
+	cout << "row?: ";
+	cin  >> row;
 }
 
 
@@ -130,27 +142,27 @@ void input_chocolate_bar_size(
 
 // 板チョコを表示する
 void print_chocolate_bar(
-	int row,
-	int column
+		int column,
+		int row
 )
 {
 	// 板チョコの頭を表示する
-	print_chocolate_bar_head( row );
+	print_chocolate_bar_head( column );
 
 
 	// 板チョコの中身を、
 	// 縦の数だけ表示する
-	if (column < 0)
+	if (row < 0)
 	{
 		cout << "縦の数がマイナスです！" << endl;
 	}
-	for (int i = 0; i < column; ++i)
+	for (int i = 0; i < row; ++i)
 	{
-		print_chocolate_bar_body( row );
+		print_chocolate_bar_body( column );
 	}
 
 	// 板チョコの包装紙を表示する
-	print_chocolate_bar_wrapping( row );
+	print_chocolate_bar_wrapping( column );
 }
 
 
@@ -158,11 +170,11 @@ void print_chocolate_bar(
 
 // 板チョコの頭を表示する
 void print_chocolate_bar_head(
-	int row
+		int column
 )
 {
-	print_3_string_row_times(
-		row,
+	print_chocolate_bar_line(
+		column,
 		"┏ ",  "━ ━ ━ ",  "┓ "
 	);
 }
@@ -172,21 +184,21 @@ void print_chocolate_bar_head(
 
 // 板チョコの中身を表示する
 void print_chocolate_bar_body(
-	int row
+		int column
 )
 {
-	print_3_string_row_times(
-		row,
+	print_chocolate_bar_line(
+		column,
 		"┃ ",  "┏ ━ ┓ ",  "┃ "
 	);
 	
-	print_3_string_row_times(
-		row,
+	print_chocolate_bar_line(
+		column,
 		"┃ ",  "┃ 　┃ " ,  "┃ "
 	);
 	
-	print_3_string_row_times(
-		row,
+	print_chocolate_bar_line(
+		column,
 		"┃ ",  "┗ ━ ┛ ",  "┃ "
 	);
 }
@@ -196,11 +208,11 @@ void print_chocolate_bar_body(
 
 // 板チョコの包装紙を表示する
 void print_chocolate_bar_wrapping(
-	int row
+		int column
 )
 {
-	print_3_string_row_times_with_decoration(
-		row,
+	print_chocolate_bar_line_decorated(
+		column,
 		"┣ "      ,  "┣ ┯ ",
 		"━ ━ ━ ",  "━ "   ,
 		"┫ "      ,  "┯ ┫ "
@@ -208,23 +220,23 @@ void print_chocolate_bar_wrapping(
 	
 	for (int i = 0; i < WRAPPING_HEIGHT; ++i)
 	{
-		print_3_string_row_times_with_decoration(
-			row,
+		print_chocolate_bar_line_decorated(
+			column,
 			"┃ "      ,  "┃ │ ",
 			"　　　"   ,  "　"    ,
 			"┃ "      ,  "│ ┃ "
 		);
 	}
 	
-	print_3_string_row_times_with_decoration(
-		row,
+	print_chocolate_bar_line_decorated(
+		column,
 		"┃ "      ,  "┃ └ ",
 		"─ ─ ─ ",  "─ "   ,
 		"┃ "      ,  "┘ ┃ "
 	);
 	
-	print_3_string_row_times_with_decoration(
-		row,
+	print_chocolate_bar_line_decorated(
+		column,
 		"┗ "      ,  "┗ ━ ",
 		"━ ━ ━ ",  "━ "   ,
 		"┛ "      ,  "━ ┛ "
@@ -237,15 +249,15 @@ void print_chocolate_bar_wrapping(
 
 
 // 左端+真ん中+右端で文字列を表示する
-void print_3_string_row_times(
-	int    row,
-	string left,
-	string center,
-	string right
+void print_chocolate_bar_line(
+		int    column,
+		string left,
+		string center,
+		string right
 )
 {
 	cout << left;
-	for (int i = 0; i < row; ++i)
+	for (int i = 0; i < column; ++i)
 	{
 		cout << center;
 	}
@@ -257,28 +269,28 @@ void print_3_string_row_times(
 
 
 // 左端+真ん中+右端で飾り付きの部分の文字列を表示する
-void print_3_string_row_times_with_decoration(
-	int    row,
-	string left,
-	string left_decolated,
-	string center,
-	string center_adjust,
-	string right,
-	string right_decolated
+void print_chocolate_bar_line_decorated(
+		int    column,
+		string left,
+		string left_decolated,
+		string center,
+		string center_adjust,
+		string right,
+		string right_decolated
 )
 {
 	// 横幅が1未満(模様を表示する幅が無い)の場合
-	if (row < 1)
+	if (column < 1)
 	{
-		print_3_string_row_times(
-			row,
+		print_chocolate_bar_line(
+			column,
 			left,  center,  right
 		);
 	}
 	else
 	{
 		cout << left_decolated;
-		for (int i = 0; i < row - 1; ++i)
+		for (int i = 0; i < column - 1; ++i)
 		{
 			cout << center;
 		}
